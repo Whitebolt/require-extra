d9fee2584b55b5ce45e3e3f5cead00f1514709be
Stephen Simpson <stephen@whitebolt.net>
2015-10-03 11:51:18 +0100

    Add test bash script to export jsDoc and run Mocha.
    Exports jsDoc fromindex so this can be used in test describe clauses.  Created script for doing the export as cannot be done live inside mocha test as mocha loads all the describe descriptions first.  Added a mocha test stub.  Added the exported jsdoc file to git.

---
+++ bin/jsDoc.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var fs = require('fs');
+var jsdocParse = require("jsdoc-parse");
+
+function parseJsDoc(src, callback) {
+  var txt = '';
+
+  jsdocParse({
+    src: src,
+    private: true
+  }).on('data', function(chunk) {
+    txt += chunk.toString();
+  }).on('end', function() {
+    var data = JSON.parse(txt.replace(/(?:[\n\f\r\t ]|\\n|\\r|\\t|\\f)+/g, ' '));
+    var functions = {};
+
+    data.forEach(function(item) {
+      functions[item.id] = item;
+      delete item.id;
+    });
+
+    callback(functions);
+  });
+}
+
+parseJsDoc(__dirname + '/../index.js', function(functions){
+  fs.writeFile('./test/index.json', JSON.stringify(functions), function (err) {
+  });
+});
---
+++ test/index.js
@@ -0,0 +1,32 @@
+'use strict';
+
+var path = require('path');
+var packageInfo = require('../package.json');
+var jsDoc = require('./index.json');
+
+/**
+ * Generate a description for a describe clause using the info in an object.
+ *
+ * @private
+ * @param {Object} items        The object to get a description from.
+ * @param {string} [itemName]   If supplied the property of items to get from.
+ * @returns {string}
+ */
+function describeItem(items, itemName) {
+  if(itemName){
+    return items[itemName].name + '(): ' + items[itemName].description;
+  }
+
+  return items.name + ': ' + items.description;
+}
+
+
+describe(describeItem(packageInfo), function() {
+  describe(describeItem(jsDoc, 'requireAsync'), function() {
+    it('requireX', function() {
+      // STUB
+    });
+  });
+});
+
+
