b1696be43658334350a8b2462c330d59eb287c30
Stephen Simpson <stephen@whitebolt.net>
2015-10-03 15:11:27 +0100

    Add tests to test module loading

---
+++ forTests/testModuleWithError.js
@@ -0,0 +1,9 @@
+/* jshint node: true */
+
+'use strict';
+
+var error = b/100; // jshint ignore:line
+
+module.exports = {
+  'testParam': 1
+};
--- test/index.js
+++ test/index.js
@@ -1,10 +1,14 @@
-/*jshint node: true, mocha: true */
+/* jshint node: true, mocha: true */
+/* global chai */
 
 
 'use strict';
 
 var packageInfo = require('../package.json');
 var jsDoc = require('./index.json');
+var requireX = require('../index.js');
+var expect = require('chai').expect;
+
 
 /**
  * Generate a description for a describe clause using the info in an object.
@@ -24,9 +28,76 @@ function describeItem(items, itemName) {
 
 
 describe(describeItem(packageInfo), function() {
+  it('Should export a function with 3 method: resolve, getModule and getResolver', function() {
+    expect(requireX).to.be.a('function');
+    ['resolve', 'getModule', 'getResolver'].forEach(function(method) {
+      expect(requireX[method]).to.be.a('function');
+    });
+  });
+
   describe(describeItem(jsDoc, 'requireAsync'), function() {
-    it('', function() {
-      // STUB
+    describe('Should load module asynchronously', function() {
+      it('Should return the module in node-style callback', function(done) {
+        requireX('../forTests/testModule1.js', function(error, testModule1) {
+          expect(testModule1.testParam).to.equal(1);
+          expect(error).to.equal(null);
+          done();
+        });
+      });
+
+      it('Should resolve the module to returned promise', function(done) {
+        requireX('../forTests/testModule1.js').then(function(testModule1) {
+          expect(testModule1.testParam).to.equal(1);
+          done();
+        });
+      });
+
+      it('Should return an error to node-style callback when module not found', function(done) {
+        requireX('../forTests/testModule-1.js', function(error, testModule1) {
+          expect(error).to.not.equal(null);
+          expect(testModule1).to.equal(undefined);
+          done();
+        });
+      });
+
+      it('Should reject the returned promise when module not found', function(done) {
+        requireX('../forTests/testModule-1.js').then(null, function(error) {
+          expect(error).to.not.equal(null);
+          done();
+        });
+      });
+    });
+
+    it('Should reject the promise with error when error occurs in module', function(done) {
+      requireX('../forTests/testModuleWithError.js').then(null, function(error) {
+        expect(error).to.not.equal(null);
+        done();
+      });
+    });
+
+    describe('Should load an array of modules asynchronously', function() {
+      it('Should resolve the modules to returned promise', function(done) {
+        requireX([
+          '../forTests/testModule1.js',
+          '../forTests/testModule2.js'
+        ]).spread(function(testModule1, testModule2) {
+          expect(testModule1.testParam).to.equal(1);
+          expect(testModule2.testParam).to.equal(2);
+          done();
+        });
+      });
+
+      it('Should return modules in callback', function(done) {
+        requireX([
+          '../forTests/testModule1.js',
+          '../forTests/testModule2.js'
+        ], function(error, testModule1, testModule2) {
+          expect(error).to.equal(null);
+          expect(testModule1.testParam).to.equal(1);
+          expect(testModule2.testParam).to.equal(2);
+          done();
+        });
+      });
     });
   });
 
